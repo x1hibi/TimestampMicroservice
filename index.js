f223887afebdc6fbbcfde6b4b6d3c1d01165408e
x1hibi Alvarado <kamisama@comunidad.unam.mx>
2025-04-13 03:23:45 +0000

    x1hibi: TimeStamp microservice (done)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,10 +19,41 @@ app.get("/", function (req, res) {
 });
 
 
+
+
+function getDateApi(request, response) {
+  // Get parameters from URL
+  let dateString = request.params.date
+  // Validate with regex date string
+  let isDateInMiliseconds = /^\d+$/.test(dateString)
+  let dateResponse = {}
+  // Evaluate if date are in miliseconds
+  if (isDateInMiliseconds) {
+    console.log("miliseconds:", dateString)
+    dateResponse.unix = parseInt(dateString)
+    dateResponse.utc = new Date(dateResponse.unix).toUTCString()
+  // Evaluate if string is empty
+  }else if(dateString == undefined){
+    let date = new Date()
+      dateResponse.unix = date.getTime()
+      dateResponse.utc = date.toUTCString()
+  // Evaluate if date string is valid or not
+  }else {
+    date = new Date(dateString)
+    if (date == "Invalid Date") {
+      dateResponse.error = "Invalid Date"
+    }else{
+      let date = new Date(dateString)
+      dateResponse.unix = date.getTime()
+      dateResponse.utc = date.toUTCString()
+    }
+  }
+  // Return date in seconds and UTC formart
+  response.json(dateResponse)
+}
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api/:date?", getDateApi);
 
 
 
